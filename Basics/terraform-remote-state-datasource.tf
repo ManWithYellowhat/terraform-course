c634808686034b00de1fe42a43c70e0654427098
basinforcode <codebasin@outlook.de>
2022-06-14 01:10:16 +0200

    remote state datasource deleted

--- Basics/terraform-remote-state-datasource.tf
+++
@@ -1,8 +0,0 @@
-data "terraform_remote_state" "vpc" {
-    backend = "s3"
-    config = {
-        bucket = "terraform-on-aws-for-ec2"
-        key = "dev/terraform.tfstate"
-        region = "us-east-1"
-    }
-}
