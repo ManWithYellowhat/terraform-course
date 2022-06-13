a4abe9026ab1d8b4747b1ae98453d0cd2235e44f
basinforcode <codebasin@outlook.de>
2022-06-14 01:14:19 +0200

    ec-instnace renaming

--- Basics/ec2-instance.tf
+++ Basics/ec2-instance.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "ec2demo" {
+resource "aws_instance" "myec2vm" {
     ami = data.aws_ami.amzlinux2.id
     instance_type = var.instance_type
     user_data = file("${path.module}/app1-install.sh")
