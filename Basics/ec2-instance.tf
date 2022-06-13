f7bf7f58d70d835395bfcf05d9c75c21ac51da4b
basinforcode <codebasin@outlook.de>
2022-06-14 00:56:43 +0200

    publicip and publicdns output

--- Basics/ec2-instance.tf
+++ Basics/ec2-instance.tf
@@ -1,10 +1,13 @@
 resource "aws_instance" "ec2demo" {
     ami = data.aws_ami.amzlinux2.id
     instance_type = var.instance_type
-    key_name = "terraform-key"
-    user_data = file("apache-install.sh")
-    vpc_security_group_ids = [aws_security_group]
+    user_data = file("${path.module}/apache-install.sh")
+    key_name = var.instance_keypair
+    vpc_security_group_ids = [
+        aws_security_group.vpc-ssh.id,
+        aws_security_group.vpc-web.id
+    ]
     tags = {
-        "Name" = "amz-linux-vm"
+        "Name" = "EC2 Demo 2"
     }
 }
