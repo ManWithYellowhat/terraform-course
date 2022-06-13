9155eb071d5e9575c2758628db911a999bd03c24
basinforcode <codebasin@outlook.de>
2022-06-13 15:07:32 +0200

    clean up

--- Basics/ec2-instance.tf
+++ Basics/ec2-instance.tf
@@ -1,22 +1,11 @@
-terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-        }
-    }
-}
-
-provider "aws" {
-    profile = "default"
-    region = "us-east-1"
-}
-
 resource "aws_instance" "ec2demo" {
     ami = "ami-0022f774911c1d690"
     instance_type = "t2.micro"
-    /*
-    This is a
-    multiline
-    comment
-    */
+
+    key_name = "terraform_key"
+    user_data = file("apache-install.sh")
+    vpc_security_group_ids = [aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id]
+    tags = {
+        "Name" = "amz-linux-vm"
+    }
 }
