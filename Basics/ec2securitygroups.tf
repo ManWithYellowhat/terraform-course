97084fe44d0622be45ee75fd2acdfd383d106179
basinforcode <codebasin@outlook.de>
2022-06-14 01:07:02 +0200

    revision ec2secgroups

--- Basics/ec2securitygroups.tf
+++ Basics/ec2securitygroups.tf
@@ -1,5 +1,5 @@
 resource "aws_security_group" "vpc-ssh" {
-  name = "allow_tls"
+  name = "vpc-ssh"
   description = "Dev VPC SSH"
 
   ingress {
@@ -7,10 +7,11 @@ resource "aws_security_group" "vpc-ssh" {
     from_port = 22
     to_port = 22
     protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0", ]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
+    description = "Allow all ip and ports outbound"
     from_port = 0
     to_port = 0
     protocol = "-1"
@@ -23,21 +24,21 @@ resource "aws_security_group" "vpc-ssh" {
 }
 
 resource "aws_security_group" "vpc-web" {
-  name = "allow_tls"
+  name = "vpc-web"
   description = "Dev VPC Web"
 
   ingress {
     description = "Allow Port 22"
-    from_port = 22
-    to_port = 22
+    from_port = 80
+    to_port = 80
     protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0", ]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     description = "Allow Port 443"
-    from_port = 0
-    to_port = 0
+    from_port = 443
+    to_port = 443
     protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
