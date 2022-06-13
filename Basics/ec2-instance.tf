57fbd66f1f09b34d90e01bd62b2ba85e3e4cffdd
basinforcode <codebasin@outlook.de>
2022-06-14 01:11:43 +0200

    ec2-instance.tf revision

--- Basics/ec2-instance.tf
+++ Basics/ec2-instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2demo" {
     ami = data.aws_ami.amzlinux2.id
     instance_type = var.instance_type
-    user_data = file("${path.module}/apache-install.sh")
+    user_data = file("${path.module}/app1-install.sh")
     key_name = var.instance_keypair
     vpc_security_group_ids = [
         aws_security_group.vpc-ssh.id,
